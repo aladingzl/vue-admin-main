81460a9dec3bc4876dbe7463ed78a8532ca38a77
aladingzl <2414151633@qq.com>
2021-11-25 15:46:14 +0800

    feat: axios加入拦截器封装

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,11 +1,36 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { AxiosInstance, AxiosRequestConfig } from 'axios'
 // 类的封装性更强
+interface MYRequestInterceptors {
+  requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  requestInterceptorCatch?: (error: any) => any
+
+  responseInterceptor?: (res: AxiosResponse) => AxiosRequestConfig
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  responseInterceptorCatch?: (error: any) => any
+}
+
+interface MYRequestConfig extends AxiosRequestConfig {
+  interceptors?: MYRequestInterceptors
+}
 class MYRequest {
   instance: AxiosInstance
+  interceptors?: MYRequestInterceptors
 
-  constructor(config: AxiosRequestConfig) {
+  constructor(config: MYRequestConfig) {
     this.instance = axios.create(config)
+    this.interceptors = config.interceptors
+
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
+
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.responseInterceptorCatch
+    )
   }
 
   request(config: AxiosRequestConfig): void {
