4404cca31d28f8b0d080143a6721d28a9bd96dc2
aladingzl <2414151633@qq.com>
2021-11-29 22:09:08 +0800

    feat: 首页基本结构搭建

---
+++ src/components/nav-header/src/nav-header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="nav-header"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style scoped lang="less">
+.nav-header {
+  .fold-menu {
+    font-size: 30px;
+    cursor: pointer;
+  }
+}
+</style>
